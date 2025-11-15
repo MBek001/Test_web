777650157c55ebcc3cddc152a5e3c9be52620dde
mbek <mmm857436@gmail.com>
2025-11-15 21:13:12 +0000

    2

--- quiz_platform/settings.py
+++ quiz_platform/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "quiz_platform.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.test_web",
     }
 }
 
